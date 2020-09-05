d9adb7b48432721145d12c13527697bf89322696
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-05 21:17:06 +0430

    use log middleware

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,21 @@ const port: number = PORT || 3000;
 
 const app: expressive.App = new expressive.App();
 
+app.use(
+  async (
+    req: expressive.Request,
+    res: expressive.Response,
+    next: () => Promise<void>
+  ) => {
+    const startTime = Date.now();
+
+    await next();
+
+    const reqTime = Date.now() - startTime + "ms";
+
+    log.info(res.status, req.method, req.url, reqTime);
+  }
+);
+
 const server = await app.listen(port);
 log.info(`> app listening at http://127.0.0.1:${port}`);
