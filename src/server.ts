97592827b9acf5a0e786267d4a23e35766d9340b
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-05 22:39:06 +0430

    get all data

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import { expressive, log } from "./deps.ts";
 
+import DataST from "./DataST.ts";
 import { PORT } from "./config.ts";
 
 const port: number = PORT || 3000;
 
 const app: expressive.App = new expressive.App();
-
-app.use(expressive.bodyParser.json());
 
 app.use(
   async (
@@ -23,6 +22,13 @@ app.use(
     log.info(res.status, req.method, req.url, reqTime);
   }
 );
+
+app.get("/", async (req: expressive.Request, res: expressive.Response) => {
+  await res.json({
+    status: "success",
+    data: DataST.getInstance().getSimpleData(),
+  });
+});
 
 const server = await app.listen(port);
 log.info(`> app listening at http://127.0.0.1:${port}`);
